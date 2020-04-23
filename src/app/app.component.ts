65cfca0b6d517c47d7328b405fd62a774d80c6ff
Aemal Shirzai <aemalshirzai2016@gmail.com>
2020-04-24 00:05:18 +0430

    solved the backdrop problem of bootstrap in back button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { PlatformLocation } from '@angular/common'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,6 +8,17 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'covid19live';
 
+  constructor(location: PlatformLocation) {
+    location.onPopState(() => {
+    
+      // Array.from(document.getElementsByClassName("modal") as HTMLCollectionOf<HTMLElement>)[0].style.display = "none";
+      document.getElementsByClassName("modal-backdrop")[0].remove();
+      document.getElementsByTagName("BODY")[0].classList.remove("modal-open")
+      // Array.from(document.getElementsByClassName("modal-backdrop") as HTMLCollectionOf<HTMLElement>)[0].style.display = "none";
+      
+
+  });
+  }
   
 }
 
